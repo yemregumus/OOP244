9cd86813e2df54f9c037a569e705b3dcd5c9bfeb
Yunus Emre Gumus <ygumus@myseneca.ca>
2022-04-26 14:38:22 -0400

    Add files via upload

---
+++ ws1/P1/cStrTools.h
@@ -0,0 +1,25 @@
+/* Workshop #1 Part #1
+Full name			: Yunus Gumus
+Student ID number	: 150331197
+Seneca email address: ygumus@myseneca.ca
+Section code		: NCC              */
+
+#ifndef SDDS_CSTRTOOLS_H 
+#define SDDS_CSTRTOOLS_H
+namespace sdds {
+
+
+char toLower(char ch);
+int strCmp(const char* s1, const char* s2);
+int strnCmp(const char* s1, const char* s2, int len);
+void strCpy(char* des, const char* src);
+int strLen(const char* str);
+const char* strStr(const char* str, const char* find);
+int isAlpha(char ch);
+int isSpace(char ch);
+void trim(char word[]);
+void toLowerCaseAndCopy(char des[], const char source[]);
+
+
+}
+#endif
